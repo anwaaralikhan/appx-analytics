2467ae51cb8ca0e4323e4cbb0aaca55efc61db3e
anwaaralikhan <manwaaronline@gmail.com>
2020-06-03 20:12:37 -0400

    AppX Analytics Social Media Stats Service

---
+++ appx-socialmedia-service/src/main/java/com/dc/appx/socialmedia/AppxSocialmediaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dc.appx.socialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppxSocialmediaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppxSocialmediaServiceApplication.class, args);
+	}
+
+}
---
+++ appx-socialmedia-service/src/test/java/com/dc/appx/socialmedia/AppxSocialmediaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dc.appx.socialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppxSocialmediaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
