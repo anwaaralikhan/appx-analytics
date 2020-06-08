56e59c0bc330c7c860f879ab822a65193f2c7897
anwaaralikhan <manwaaronline@gmail.com>
2020-06-08 19:56:23 -0400

    AppX Eureka Discovery Server

---
+++ appx-eureka-server/src/main/java/com/dc/appx/eureka/AppxEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.dc.appx.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppxEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppxEurekaServerApplication.class, args);
+	}
+
+}
---
+++ appx-eureka-server/src/test/java/com/dc/appx/eureka/AppxEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dc.appx.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppxEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
